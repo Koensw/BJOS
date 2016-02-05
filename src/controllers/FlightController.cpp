0dd8fd1b07eb478d09a05b6cb8c6aef1706f1e04
Joep Linssen <joep@bluejayeindhoven.nl>
2016-02-05 11:53:20 +0100

    Moved takeoff and land hack to FlightController, instead of having it in the FlightController caller

--- src/controllers/FlightController.cpp
+++ src/controllers/FlightController.cpp
@@ -643,7 +643,13 @@ void FlightController::syncVision(Eigen::Vector3d visionPosEstimate, double visi
 //ALERT: can NOT be used to set roll, pitch, rollspeed or pitchspeed
 //WARNING: cannot set position at the moment (maybe rework this to use Pose / Twist?)
 void FlightController::setTargetCF(uint16_t type_mask, Eigen::Vector3d position,
-        Eigen::Vector3d orientation, Eigen::Vector3d velocity, Eigen::Vector3d angularVelocity) {	
+        Eigen::Vector3d orientation, Eigen::Vector3d velocity, Eigen::Vector3d angularVelocity) {
+
+    if (type_mask == SET_TARGET_TAKEOFF)
+        velocity = Eigen::Vector3d(0.0f, 0.0f, 2.0f);
+    if (type_mask == SET_TARGET_LAND)
+        velocity = Eigen::Vector3d(0.0f, 0.0f, -0.5f);
+    
     std::lock_guard<bjos::BJOS::Mutex> lock(*shared_data_mutex);
     /* Tranform given position, velocity and yaw from CF frame to NED Body frame */
     //Eigen::Vector3d positionNED = positionCFtoNED(position);
