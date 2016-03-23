34cd677e63136d2f8b57c665fb1d8ae59efd4d71
Bluejay Control <bluejay-control@bluejayeindhoven.nl>
2016-03-23 17:11:29 +0100

    Revert "RGBeyes more bugfixes"
    This reverts commit 84359426325876b6ac6439b14a82bb7e69a033bf.

--- src/loader/test_rgbeyes.cpp
+++ src/loader/test_rgbeyes.cpp
@@ -3,6 +3,12 @@
 #include <thread>
 #include <chrono>
 
+/*#include "../libs/clk.h"
+#include "../libs/gpio.h"
+#include "../libs/dma.h"
+#include "../libs/pwm.h"
+
+#include "../libs/ws2811.h"*/
 
 #include "bjos/bjos.h"
 #include "bjos/helpers/error.h"
