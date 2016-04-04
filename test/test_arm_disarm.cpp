ad8d275a96d45b37664e5514866d137baa888c9c
Bluejay Control <bluejay-control@bluejayeindhoven.nl>
2016-04-04 15:57:48 +0200

    Changed if-statement logic

--- test/test_arm_disarm.cpp
+++ test/test_arm_disarm.cpp
@@ -46,7 +46,7 @@ int main(){
         std::cout << "Arm = 1, disarm = 0, quit = -1: ";
         std::cin >> type;
         
-        if(type != -1 || type != 0 || type != 1)
+        if(type != -1 && type != 0 && type != 1)
             std::cout << "That ain't -1, 0 or 1. Dumbass." << std::endl;
         else
             flight.armDisarm((bool)type);
